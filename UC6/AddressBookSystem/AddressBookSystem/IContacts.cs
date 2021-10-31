8c497820d31908007b1ff3da0a5b3d3429dd9d35
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-31 23:46:08 +0530

    Added: Multiple address book with unique name for address

---
+++ UC6/AddressBookSystem/AddressBookSystem/IContacts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    interface IContacts
+    {
+        public void addContact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email);
+        public void editContact(string firstName);
+        public void deleteContact(string firstName);
+        public void displayContact();
+    }
+}
