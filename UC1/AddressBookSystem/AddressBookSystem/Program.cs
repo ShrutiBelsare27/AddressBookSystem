58c73d10ab94ae404962e9e0eebcc701d740ca4d
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-31 21:05:05 +0530

    Added: Created contact inormation of person

---
+++ UC1/AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To adress Book System!");
+
+            Program program = new Program();
+        }
+    }
+}
